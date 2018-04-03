e34b97b260e2cd6b3c1c3edc862c2f588f16689d
tnasralah <31813631+tnasralah@users.noreply.github.com>
2018-04-02 23:13:16 -0500

    Update Word_Replacer.py

--- Word_Replacer.py
+++ Word_Replacer.py
@@ -2,6 +2,26 @@
 from nltk.corpus import wordnet
 import re
 
+import enchant
+from nltk.metrics import edit_distance
+
+
+class SpellingReplacer(object):
+    def __init__(self, dict_name='en', max_dist=2):
+        self.spell_dict = enchant.Dict(dict_name)
+        self.max_dist = max_dist
+
+    def replace(self, word):
+        if self.spell_dict.check(word):
+            return word
+
+        suggestions = self.spell_dict.suggest(word)
+        if suggestions and edit_distance(word, suggestions[0]) <=self.max_dist:
+            return suggestions[0]
+        else:
+            return word
+
+
 replacement_patterns = [
 (r'won\'t', 'will not'),
 (r'can\'t', 'can not'),
