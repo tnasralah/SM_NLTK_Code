c53e39fdfdc576d9f1e154b5d29fd0d2c06921d3
Omar El-Gayar <gayaro@yahoo.com>
2018-04-09 16:45:10 -0500

    Added additional ngram exploratory code

--- ofg_experiments.py
+++ ofg_experiments.py
@@ -2,6 +2,11 @@ from nltk.corpus import PlaintextCorpusReader
 from nltk.corpus import stopwords
 from nltk.corpus.reader.util import read_line_block
 
+from nltk.stem.wordnet import WordNetLemmatizer
+
+from nltk.util import ngrams
+from nltk.probability import FreqDist
+
 import gensim
 from gensim import corpora
 
@@ -52,7 +57,7 @@ print(list(bi_grams)[:10])
 
 # Defining stop words
 stopwords = stopwords.words('english')
-add_stopwords = ['http', 'https', '://', 'www', 'com', '8800', '...', '....', 'yep', '.).', '.:).', '++..', 'github']
+add_stopwords = ['http', 'https', '://', 'www', 'com', '8800', '...', '....', 'yep', '.).', '](#', '.:).', '++..', 'github']
 [stopwords.append(st) for st in add_stopwords]
 
 
@@ -66,43 +71,55 @@ print(words)
 print(len(words))
 """
 
-"""
-Pre-processing of paragraphs
-"""
 
-
-# Filter short words and stopwords from paragraphs
+# Filter short words and stopwords from paragraphs, and lemmatize
 filtered_paras = [[] for i in range(len(ww.paras()))]
 
+lemma = WordNetLemmatizer()
 i = 0
 for p in ww.paras():
     for s in p:
         for w in s:
             if len(w) >= 3 and w.lower() not in stopwords:
-                filtered_paras[i].append(w.lower())
+                filtered_paras[i].append(lemma.lemmatize(w.lower()))
     i += 1
-print(len(filtered_paras))
-print('+++ First filetered parapgraph +++')
-print(filtered_paras[0])
+# print(len(filtered_paras))
+# print('+++ First filetered parapgraph +++')
+# print(filtered_paras[0:3])
 
-"""
+
 # Generating ngrams
+uni_grams_para = []
 bi_grams_para = []
 tri_grams_para = []
 
 # Generating ngrams in paragraphs
 for p in filtered_paras:
+    uni_grams_para.append(list(ngrams(p, 1)))
     bi_grams_para.append(list(ngrams(p, 2)))
     tri_grams_para.append(list(ngrams(p, 3)))
 
 print('\n+++ ngram in first paragraphs')
-print('++++ bi_grams +++')
+print('\n+++ uni_grams +++')
+print(uni_grams_para[:1])
+print('\n+++ bi_grams +++')
 print(bi_grams_para[:1])
-print('++++ tri_grams +++')
+print('\n+++ tri_grams +++')
 print(tri_grams_para[:1])
 
 
 # Generating ngrams across entire corpus (all paragraphs)
+# bi_grams
+uni_grams = []
+for p in uni_grams_para:
+    [uni_grams.append(bg) for bg in p]
+print(uni_grams[:10])
+
+uni_gramf = FreqDist(uni_grams)
+print('\n+++ Frequent uni_grams')
+print(uni_gramf.most_common(20))
+uni_gramf.plot(50, cumulative=False)
+
 # bi_grams
 bi_grams = []
 for p in bi_grams_para:
@@ -112,7 +129,7 @@ print(bi_grams[:10])
 bi_gramf = FreqDist(bi_grams)
 print('\n+++ Frequent bi_grams')
 print(bi_gramf.most_common(20))
-bi_gramf.plot(50, cumulative=True)
+bi_gramf.plot(50, cumulative=False)
 
 # tri_grams
 tri_grams = []
@@ -123,10 +140,10 @@ print(tri_grams[:10])
 tri_gramf = FreqDist(tri_grams)
 print('\n+++ Frequent tri_grams')
 print(tri_gramf.most_common(20))
-tri_gramf.plot(50, cumulative=True)
+tri_gramf.plot(50, cumulative=False)
+
+
 """
-
-
 # Preparing Document-Term Matrix
 
 # Creating the term dictionary of our courpus, where every unique term is assigned an index.
@@ -137,7 +154,6 @@ doc_term_matrix = [dictionary.doc2bow(p) for p in filtered_paras]
 print('Number of items in dictionary = ', len(dictionary.iteritems()))
 print('Number of paragraphs = ', len(doc_term_matrix))
 
-"""
 # Exploring the doc_term_matrix
 # doc_term_matrix is a list of lists
 # Each document/paragraph is a list of tuples. Each tuple represents an index to the corresponding
@@ -160,7 +176,7 @@ print('\n+++')
 print(doc_term_matrix[0][0])
 """
 
-
+"""
 # Running LDA Model
 # Creating the object for LDA model using gensim library
 Lda = gensim.models.ldamodel.LdaModel
@@ -168,3 +184,4 @@ Lda = gensim.models.ldamodel.LdaModel
 # Running and Training LDA model on the document term matrix.
 ldamodel = Lda(doc_term_matrix, num_topics=3, id2word=dictionary, passes=50)
 print(ldamodel.print_topics(num_topics=3, num_words=3))
+"""
